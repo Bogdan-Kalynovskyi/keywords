87d77d0a093fe492cb8e51f1efbb0ea9b3ec16d1
easy-one <mybodya@gmail.com>
2017-02-27 13:14:13 +0100

    settings

---
+++ settings/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**    TODO: delete api/install.php file after installation   **/
+
+
+
+$google_api_id = '211499477101-d78crq8gs6sojr7grdlm9ebmoltiel71.apps.googleusercontent.com';
+
+
+
+
+$db_host = '127.0.0.1';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'keywords';
+
+
+$error_reporting_level = E_ALL ^ E_DEPRECATED;
+
+/***************************************************************
+ **    TODO: for production set to ZERO!    **/
+//    error_reporting(0);
+
+error_reporting($error_reporting_level);
